1db6c8bc5e5f3386546da823f6b9735447b648ea
Lorenzo <lorenzoboda560@gmail.com>
2025-02-15 15:16:56 +0100

    Resultado del programa

---
+++ README.md
@@ -0,0 +1,3 @@
+# Eligiendo la sede del mundial
+
+Este es el ejercicio A de Programame de 2023/2024.
--- src/SedeMundial.java
+++ src/SedeMundial.java
@@ -1,2 +1,67 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class SedeMundial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int votosTotales;
+
+        while (true) {
+            try {
+                votosTotales = Integer.parseInt(sc.nextLine());
+
+                if (votosTotales == 0) {
+                    break;
+                }
+
+                String paisesTotales = sc.nextLine();
+
+                if (paisesTotales.trim().isEmpty()) {
+                    System.out.println("No se han ingresado países. Intenta nuevamente.");
+                    continue;
+                }
+
+                String[] paises = paisesTotales.split(" ");
+
+
+                Map<String, Integer> mapaPaises = new LinkedHashMap<>();
+
+                for (String paise : paises) {
+                    if (!mapaPaises.containsKey(paise)) {
+                        mapaPaises.put(paise, 1);
+                    } else{
+                        mapaPaises.put(paise, mapaPaises.get(paise) + 1);
+                    }
+                }
+
+
+                String paisMaxVoto = "";
+                int maxVoto = -1;
+                boolean empate = false;
+
+                for (Map.Entry<String, Integer> entry : mapaPaises.entrySet()) {
+                    if (entry.getValue() > maxVoto) {
+                        maxVoto = entry.getValue();
+                        paisMaxVoto = entry.getKey();
+                        empate = false;
+                    } else if (entry.getValue() == maxVoto) {
+                        empate = true;
+                    }
+                }
+
+
+                if (empate) {
+                    System.out.println("EMPATE");
+                } else {
+                    System.out.println(paisMaxVoto);
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("Entrada inválida para el número de votos. Intenta nuevamente.");
+            }
+        }
+        sc.close();
+    }
 }
